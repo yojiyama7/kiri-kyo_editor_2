721541aab913199bd210babca6e67af213fefd10
yojiyama7 <yojiyama7@gmail.com>
2026-09-04 15:37:51 +0900

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: '/kiri-kyo_editor_2/',
   plugins: [svelte()]
 });
